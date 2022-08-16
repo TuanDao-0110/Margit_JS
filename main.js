36332a858fbb3f06394b9e7a460ea1f04cecc957
tuan <rickytuan1991@gmail.com>
2022-08-16 14:56:24 +0300

    done with some more note

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+//1. create  a form to ask user to input the age value
 let submitForm = document.getElementById("age_form");
-
+// 2. submit the age value to the systerm
 submitForm.addEventListener("submit", (e) => {
   let age;
+  // 2.1 prevent the page reload againt
   e.preventDefault();
+
   age = document.getElementById("input_age").value;
+  // 2.1 check that user insert value or not ?
   age ? checkValue(age) : alert("please insert your age");
 });
+//3. check the value
 const checkValue = (age) => {
   switch (true) {
     case age < 18:

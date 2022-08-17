33e894e5009db1af3a08a4c8b3d786902cd8aa60
tuan <rickytuan1991@gmail.com>
2022-08-17 13:08:25 +0300

    done

--- main.js
+++ main.js
@@ -1,61 +1,32 @@
-// Make a program that asks for three numbers from the user. The program prints out the sum of the numbers if any of the numbers are positive. If all the numbers are positive, also print out multiplication. If all numbers are negative, the text “only negatives” is printed. Handle zero as positive in this assignment.
-
+//  make :2 variable ==>
+//  1 hourly_salary vs 2. how many hours worker work in one day
+// salary calculate in this formula : 1st 7hours is normal salary, 2. after 2 hours is 50% extra. 3. rest is 100%
 //1. create  a form to ask user to input the age value
 let submitForm = document.getElementById("random_form");
+let result = document.getElementById("result");
 // 2. submit the age value to the systerm
 submitForm.addEventListener("submit", (e) => {
-  let threeNumber = [(firstNumber = 0), (secondNumber = 0), (lastNumber = 0)];
-  let numberPosNumber;
-  let result = document.getElementById("result");
-  // 2.1 prevent the page reload againt
   e.preventDefault();
-
-  numberArr = document.getElementsByClassName("number");
-
-  for (let i = 0; i < numberArr.length; i++) {
-    numberArr[i].value
-      ? (threeNumber[i] = numberArr[i].value)
-      : alert("please input value of " + (i + 1) + " number");
-  }
-  numberPosNumber = checkValue(threeNumber);
-  if (numberPosNumber === 3)
-    return (result.innerHTML = valueAllPosNumber(threeNumber));
-  if (numberPosNumber < 3 && numberPosNumber > 0)
-    return (result.innerHTML = valueOnePosNumber(threeNumber));
-  if (numberPosNumber === 0) return (result.innerHTML = valueAllNevNumber());
+  let salary = document.getElementById("salary").value;
+  let hour = document.getElementById("hour").value;
+  result.innerHTML = calulateSalary(parseInt(hour), parseInt(salary));
 });
-//3. check the value
-const checkValue = (threeNumber) => {
-  let numberPosNumber = 0;
 
-  threeNumber.map((number, index) => {
-    number > 0 ? numberPosNumber++ : "";
-  });
-
-  return numberPosNumber;
-};
-
-// 3.1 function if all number is possitive
-
-const valueAllPosNumber = (threeNumber) => {
-  let result = 1;
-  threeNumber.map((number, index) => {
-    result *= number;
-  });
-  return result;
-};
-
-// 3.2 function if all number is negative
-
-const valueAllNevNumber = () => {
-  return "only Negative";
-};
-
-// 3.3  function if one number is possitive
-const valueOnePosNumber = (threeNumber) => {
-  let result = 0;
-  threeNumber.map((number, index) => {
-    result += parseInt(number, 10);
-  });
-  return result;
+// 3. calculate your salary :
+const calulateSalary = (h, salary) => {
+  let wave = 0;
+  switch (true) {
+    case h <= 7:
+      console.log("run");
+      wave = h * salary;
+      break;
+    case h > 7 && h <= 9:
+      wave = 7 * salary + (h - 7) * salary * 1.5;
+      break;
+    default:
+      console.log("done");
+      wave = 7 * salary + 2 * salary * 1.5 + (h - 9) * 2 * salary;
+      break;
+  }
+  return wave;
 };

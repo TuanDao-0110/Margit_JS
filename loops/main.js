eb2af7565bfae96a5ce134d0f40c73cb651fa23a
tuan <rickytuan1991@gmail.com>
2022-08-23 12:53:22 +0300

    done

--- loops/main.js
+++ loops/main.js
@@ -3,26 +3,31 @@
 //1. Make a program that prints all positive numbers that are odd and smaller than 100 starting from 1. (1 3 5 7 9 11…)
 const positiveOddNum = () => {
   for (let i = 0; i < 100; i++) {
-    i % 2 != 0 ? console.log(i) : "";
+    i % 2 != 0 && console.log(i);
   }
 };
 // 2. Make a program that prints all positive numbers that are smaller than 100 and even, in this order: 2 98 4 96 6 94 … Print result in one line.
 
 const positiveEvenNum = () => {
-  let result = 0;
-  end = 0;
-  for (let i = 0; i < 100; i + 2) {
+  let result = 2;
+  number = 2;
+  for (i = 98; i > 52; i -= 2) {
     //    i % 2 == 0 && i != 0 ? console.log(i) : "";
-    result = "";
+    number += 2;
+    if (i % 2 == 0) {
+      result += " " + i + " " + number;
+
+      i == 54 && console.log(result);
+    }
   }
 };
-positiveEvenNum();
+
 //3. Make a program that asks repeatedly from the user the distance (km) and time (h) and calculates average speed. The program ends when user gives 0 for the distance. (After getting 0, the program does not ask anything from the user.)
 const askingSpeedAverage = () => {
   let average;
   let distance = 0;
   let time = 0;
-  let flag = true;
+  let flag;
   do {
     distanceTemp = prompt("what distance you are");
     if (distanceTemp != 0) {
@@ -30,6 +35,7 @@ const askingSpeedAverage = () => {
       distance += parseInt(distanceTemp);
       time += parseInt(timeTemp);
       average = distance / time;
+      console.log("average:  " + average.toFixed(2) + " km/h");
     }
     if (distanceTemp == 0) {
       flag = false;
@@ -37,14 +43,15 @@ const askingSpeedAverage = () => {
     }
   } while (flag);
 };
+// askingSpeedAverage();
 //4. Make a program that asks 20 numbers from user. After that the program prints out how many of those numbers where even.
 const asking20Num = () => {
   let result = 0;
   for (i = 1; i <= 20; i++) {
     let num = prompt("please insert your number");
-    num % 2 == 0 ? result++ : "";
+    num % 2 == 0 && result++;
 
-    i == 20 ? console.log("final even number is :" + result) : "";
+    i == 20 && console.log("total final even number is :" + result);
   }
 };
 
@@ -65,7 +72,6 @@ const averageCalculator = () => {
     }
   }
 };
-
 // 6.Make a program that asks 25 numbers form the user. In the end program prints out average of the numbers.
 
 const asking25Num = () => {
@@ -75,7 +81,7 @@ const asking25Num = () => {
     let num = prompt("please insert your number");
     total += parseInt(num);
     average = total / i;
-    i == 20 ? console.log("final average is :" + average) : "";
+    i == 20 && console.log("final average is :" + average);
   }
 };
 
@@ -98,7 +104,7 @@ const askingAverageNumber = () => {
     if (!secondQuestion) return alert("Average Number is : " + average);
   }
 };
-
+askingAverageNumber();
 // 8.Make a program that asks first how many numbers user wants to give to the program.
 //  After that program asks those numbers. In the end program prints out the smallest number that user gave.
 const askingSmallestNum = () => {

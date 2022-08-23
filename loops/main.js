921474659e994f0e019f59f181ec91af44090e80
tuan <rickytuan1991@gmail.com>
2022-08-23 13:25:31 +0300

    done

--- loops/main.js
+++ loops/main.js
@@ -1,1 +1,14 @@
 // doing some challenge with codewars.com
+
+var numUniqueEmails = function (emails) {
+  let localName;
+  let domainName;
+  for (i = 0; i < emails.length; i++) {
+    if (emails[i] == "@") {
+      localName = emails.substring(0, i - 1);
+      domainName = emails.substring(i + 1, emails.length);
+      console.log(localName + " " + domainName);
+    }
+  }
+};
+numUniqueEmails("email@mail.");

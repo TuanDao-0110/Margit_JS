d8f00f8eca9ec0a28c1a1c1876d69a1efa049e68
tuan <rickytuan1991@gmail.com>
2022-09-06 06:46:10 +0300

    still something with label active class

---
+++ changing_color/main.js
@@ -0,0 +1,50 @@
+let butObject = document.querySelectorAll('button')
+let btns = document.getElementsByClassName("btn");
+let form = document.querySelector('form')
+let color_1 = document.querySelector('#color_1')
+let color_2 = document.querySelector('#color_2')
+let color_show = document.querySelector('#color_show')
+let result = document.querySelector('#result')
+let objectStyle = {
+    direction: 'bottom right',
+    color_1Value: '#aaa',
+    color_2Value: '#aaa'
+}
+
+// let body = document.body
+
+color_1.addEventListener('input', (e) => {
+    objectStyle.color_1Value = e.target.value
+    let styleColor = `linear-gradient(to ${objectStyle.direction} , ${objectStyle.color_1Value}, ${objectStyle.color_2Value})`
+    color_show.style.background = styleColor
+    result.textContent = styleColor
+})
+color_2.addEventListener('input', (e) => {
+    objectStyle.color_2Value = e.target.value
+    let styleColor = `linear-gradient(to ${objectStyle.direction} , ${objectStyle.color_1Value}, ${objectStyle.color_2Value})`
+    color_show.style.background = styleColor
+    result.textContent = styleColor
+
+})
+const setDirection = (buttonValue) => {
+    objectStyle.direction = buttonValue
+    let styleColor = `linear-gradient(to ${objectStyle.direction} , ${objectStyle.color_1Value}, ${objectStyle.color_2Value})`
+    color_show.style.background = styleColor
+    result.textContent = styleColor
+
+}
+
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function (e) {
+        form.addEventListener('submit', e => e.preventDefault())
+        setDirection(e.target.value)
+        let current = document.getElementsByClassName("active");
+        console.log(this)
+        current[0].className = current[0].className.replace(" active", "");
+        // this.className += " active";
+        
+    });
+}
+
+
